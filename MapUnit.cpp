9bf6c53671da6a4ba82b1c0f3fd92a19322dec5b
Allen Yang <allen1759@gmail.com>
2014-12-21 13:01:12 +0800

    remove travel_fine_ in CMapUnit.

--- MapUnit.cpp
+++ MapUnit.cpp
@@ -1,19 +1,19 @@
 #include "MapUnit.h"
 
-CMapUnit::CMapUnit(std::string name, size_t id, size_t fine, size_t numPlayer)
-        : name_(name), id_(id), travel_fine_(id)
+CMapUnit::CMapUnit(std::string name, size_t id, size_t numPlayer)
+        : name_(name), id_(id)
 {
     for(size_t i=0; i<numPlayer; i+=1) {
         who_is_here_.push_back( false );
     }
 }
 
-void CMapUnit::arriveHere(size_t player_id)
+inline void CMapUnit::arriveHere(size_t player_id)
 {
     who_is_here_[player_id] = true;
 }
 
-void CMapUnit::leaveHere(size_t player_id)
+inline void CMapUnit::leaveHere(size_t player_id)
 {
     who_is_here_[player_id] = false;
 }
--- MapUnit.h
+++ MapUnit.h
@@ -8,15 +8,18 @@
 class CMapUnit
 {
 public:
-    CMapUnit(std::string name, size_t id, size_t fine, size_t numPlayer);
+    CMapUnit(std::string name, size_t id, size_t numPlayer);
     void arriveHere(size_t player_id);
     void leaveHere(size_t player_id);
 
 protected:
     std::string name_;
-    size_t id_, travel_fine_;
+    size_t id_; //, travel_fine_;
+    // fine 放這裡不太好 因為jail不用罰金
+    // 給繼承的map多一個 travel_fine_ 好了
     std::vector<bool> who_is_here_;
     // 有在這裡就是 true , 否則就是 false
+
 private:
 };
 
